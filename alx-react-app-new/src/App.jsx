e90d6244082222173fc410acac3649b03df9ae60
FavyWorld <adeosunfavoradewole@gmail.com>
2024-08-17 16:42:52 +0100

    alx-react-app-new

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -5,13 +5,10 @@ import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 import UserProfile from './components/UserProfile'
-import InlineStyling from './InlineStyling'
 
 
 function App() {
   const [count, setCount] = useState(0)
-
-  
 
   return (
     <>
@@ -20,8 +17,6 @@ function App() {
     <MainContent />
     <Footer />
     <UserProfile name="Alice" age= "25" bio="Loves hiking and photography"/>
-    <InlineStyling/>
-
 
     </>
   )
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent(){
+    return(
+        <main style={{color:"deepskyblue"}}>
+            <p>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    )
+}
+
+export default MainContent;
