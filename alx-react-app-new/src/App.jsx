cb0e1d9b8651c47c8612a3736e277276dcb08485
FavyWorld <adeosunfavoradewole@gmail.com>
2024-08-17 17:35:40 +0100

    alx-react-app-new

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -5,6 +5,7 @@ import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 import UserProfile from './components/UserProfile'
+import Counter from './components/Counter'
 
 
 function App() {
@@ -17,7 +18,7 @@ function App() {
     <MainContent />
     <Footer />
     <UserProfile name="Alice" age= "25" bio="Loves hiking and photography"/>
-
+    <Counter/>
     </>
   )
 }
