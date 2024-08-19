6de4b40c2f2c4ea34e0f3f7145356497f61e3c32
FavyWorld <adeosunfavoradewole@gmail.com>
2024-08-19 18:29:38 +0100

    ../my-company

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer style={{ padding: '10px', backgroundColor: '#f4f4f4', textAlign: 'center' }}>
+      <p>&copy; 2024 My Company. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
