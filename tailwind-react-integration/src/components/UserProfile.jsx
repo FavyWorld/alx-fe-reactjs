4a877c5bf18e8e9757aad519de55f461c26e4eb7
FavyWorld <adeosunfavoradewole@gmail.com>
2024-09-11 11:58:00 +0100

    tailwind-react-integration

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -3,8 +3,10 @@ import React from "react";
 
 function UserProfile() {
   return (
-    <div className="bg-gray-100 p-4 sm:p-8 max-w-xs sm:max-w-sm mx-auto my-10 sm:my-20 rounded-lg shadow-lg">
-      <img src="https://via.placeholder.com/150" alt="User"
+    <div className="bg-gray-100 p-4 sm:p-4 md:p-8 max-w-xs sm:max-w-sm mx-auto my-10 sm:my-20 rounded-lg shadow-lg">
+      <img
+        src="https://via.placeholder.com/150"
+        alt="User"
         className="rounded-full w-24 h-24 sm:w-36 sm:h-36 mx-auto"
       />
       <h1 className="text-lg sm:text-xl text-blue-800 my-4 text-center">John Doe</h1>
